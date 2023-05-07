66231150d98b0e55f1d36ba321fbf20753b58b13
Mohamed Ali BELHADJ <mohamed@mozark.ai>
2023-05-07 19:33:24 +0100

    Push project

---
+++ BankingApp/Main/Controllers/AcoountDetails/AccountDetailsController.swift
@@ -0,0 +1,19 @@
+//
+//  MyAccountsController.swift
+//  BankingApp
+//
+//  Created by Mohamed Ali BELHADJ on 07/05/2023.
+//
+
+import UIKit
+
+class MyAccountsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BankingApp/Main/Controllers/AcoountDetails/AccountDetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyAccountViewModel.swift
+//  BankingApp
+//
+//  Created by Mohamed Ali BELHADJ on 07/05/2023.
+//
+
+import Foundation
